6c21f2867f681ebbf4b8ba70020c1f1e7f5133fb
Nota Name <email@thisisnotanemail.com>
2023-11-21 14:17:23 -0800

    stars

--- script.js
+++ script.js
@@ -4,49 +4,35 @@ let speed;
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('starfield');
-    for (let i = 0; i < 800; i++) {
-        stars[i] = new Star();
-    }
     speed = 1;
-    console.log("Canvas initialized with size:", windowWidth, windowHeight); // Check canvas size
+    console.log("Canvas initialized with size:", windowWidth, windowHeight);
 }
 
 function draw() {
-    console.log("Draw function executing"); // Confirm draw function execution
+    console.log("Draw function executing");
+    background(0); // Change back to black background
+
+    // Temporary: draw a fixed circle to test rendering
+    fill(255, 0, 0);
+    ellipse(width / 2, height / 2, 50, 50);
+
+    // Comment out the stars for now
+    /*
     speed = map(mouseX, 0, width, 0, 50);
-    background(255, 0, 0); // Temporarily change background color to red
     translate(width / 2, height / 2);
     for (let i = 0; i < stars.length; i++) {
         stars[i].update();
         stars[i].show();
     }
+    */
 }
 
+// Comment out the Star class for now
+/*
 function Star() {
-    this.x = random(-width, width);
-    this.y = random(-height, height);
-    this.z = random(width);
-
-    this.update = function() {
-        this.z = this.z - speed;
-        if (this.z < 1) {
-            this.z = width;
-            this.x = random(-width, width);
-            this.y = random(-height, height);
-        }
-    };
-
-    this.show = function() {
-        fill(255);
-        noStroke();
-
-        let sx = map(this.x / this.z, 0, 1, 0, width);
-        let sy = map(this.y / this.z, 0, 1, 0, height);
-
-        let r = map(this.z, 0, width, 16, 0);
-        ellipse(sx, sy, r, r);
-    };
+    // Star class definition
 }
+*/
 
 let words = ["Planet", "Star", "Galaxy", "Comet", "Asteroid"];
 let currentWordIndex = 0;
