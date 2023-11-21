67f2926d26e324e68f23abea1acf69aecf6b5172
Nota Name <email@thisisnotanemail.com>
2023-11-21 14:01:38 -0800

    201

---
+++ script.js
@@ -0,0 +1,47 @@
+let stars = [];
+let speed;
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('starfield');
+    for (let i = 0; i < 800; i++) {
+        stars[i] = new Star();
+    }
+    speed = 1;
+}
+
+function draw() {
+    speed = map(mouseX, 0, width, 0, 50);
+    background(0);
+    translate(width / 2, height / 2);
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].update();
+        stars[i].show();
+    }
+}
+
+function Star() {
+    this.x = random(-width, width);
+    this.y = random(-height, height);
+    this.z = random(width);
+
+    this.update = function() {
+        this.z = this.z - speed;
+        if (this.z < 1) {
+            this.z = width;
+            this.x = random(-width, width);
+            this.y = random(-height, height);
+        }
+    };
+
+    this.show = function() {
+        fill(255);
+        noStroke();
+
+        let sx = map(this.x / this.z, 0, 1, 0, width);
+        let sy = map(this.y / this.z, 0, 1, 0, height);
+
+        let r = map(this.z, 0, width, 16, 0);
+        ellipse(sx, sy, r, r);
+    };
+}
